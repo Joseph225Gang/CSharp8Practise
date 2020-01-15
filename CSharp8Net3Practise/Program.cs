8724965614c9d7c606ea87356cf8939ea3997bb6
Joseph <lai225joseph@gmail.com>
2020-01-15 11:55:56 +0800

    Indices  and Range

--- CSharp8Net3Practise/Program.cs
+++ CSharp8Net3Practise/Program.cs
@@ -1,15 +1,45 @@
 ﻿using System;
+using System.Linq;
 
 namespace CSharp8Net3Practise
 {
+    /// <summary>
+    /// C# 8.0 and .NET Core 3.0
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
+            #region Pattern Matching
             DoTupleExpressionDemo();
             DoSwitchExpressionDemo();
             DoPositionPatternSampleDemo();
             DoPropertyPatternDemo();
+            #endregion
+
+            #region Indices and Ranges
+            var numbers = Enumerable.Range(0, 10).ToArray();
+            var copy = numbers[0..^0];
+            var allButFirst = numbers[1..];
+            var lastItemRange = numbers[^1..];
+            var lastItem = numbers[^1];
+            var lastThree = numbers[^3];
+
+            Index middle = 4;
+            Index threeFromEnd = ^3;
+            Range customRange = middle..threeFromEnd;
+            var custom = numbers[customRange];
+
+            Console.WriteLine($"numbers: {string.Join(", ", numbers)}");
+            Console.WriteLine($"copy: {string.Join(", ", copy)}");
+            Console.WriteLine($"allButFirst: {string.Join(", ", allButFirst)}");
+            Console.WriteLine($"lastItemRange: {string.Join(", ", lastItemRange)}");
+            Console.WriteLine($"lastItem: {lastItem}");
+            Console.WriteLine($"lastThree: {string.Join(", ", lastThree)}");
+            Console.WriteLine($"customRange: {customRange}");
+            Console.WriteLine($"custom: {string.Join(", ", custom)}");
+            #endregion
+
             Console.WriteLine("Hello World!");
         }
 
